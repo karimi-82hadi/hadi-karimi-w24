d1acdf067c8eb9fb13648851e446b7e3da2a6ea9
Hadi Karimi <karimi1382hadi@gmail.com>
2024-11-06 23:40:08 +0330

    feat: Redirect HomePage to AccountPage.

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,7 +1,12 @@
-export default function Home() {
-  return (
-    <main>
-      <h1>مینی پروژه مدیریت محصولات</h1>
-    </main>
-  );
+import { useRouter } from "next/router";
+import { useEffect } from "react";
+
+export default function HomePage() {
+  const router = useRouter();
+
+  useEffect(() => {
+    router.push("/account");
+  }, []);
+
+  return <h1 style={{ display: "none" }}>مینی پروژه مدیریت محصولات</h1>;
 }
